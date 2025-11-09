8c174694f79dbbed2b84fdf7b83aa320bf0a0574
Robin Cunningham <79066805+rspcunningham@users.noreply.github.com>
2025-11-09 00:32:09 -0500

    add remaining gates

--- quantum/gates.py
+++ quantum/gates.py
@@ -1,6 +1,6 @@
 import torch
 
-__all__ = ["I", "H", "X", "Y", "Z", "RZ", "CNOT"]
+__all__ = ["I", "H", "X", "Y", "Z", "RZ", "CX", "CZ", "CCX"]
 
 # identity gate
 I = torch.tensor(
@@ -53,12 +53,29 @@ def RZ(theta: float) -> torch.Tensor:
         [0, torch.cos(half_theta) + 1j * torch.sin(half_theta)]],
         dtype=torch.complex64)
 
-# CNOT gate (2-qubit gate)
-# Basis ordering: |00⟩, |01⟩, |10⟩, |11⟩
-# CNOT flips target if control is |1⟩
-CNOT = torch.tensor(
+# CNOT aka CX
+CX = torch.tensor(
     [[1, 0, 0, 0],
      [0, 1, 0, 0],
      [0, 0, 0, 1],
      [0, 0, 1, 0]],
+    dtype=torch.complex64)
+
+CZ = torch.tensor(
+    [[1, 0, 0, 0],
+     [0, 1, 0, 0],
+     [0, 0, 1, 0],
+     [0, 0, 0, -1]],
+    dtype=torch.complex64)
+
+# CCNOT aka Toffoli aka CCX
+CCX = torch.tensor(
+    [[1, 0, 0, 0, 0, 0, 0, 0],
+     [0, 1, 0, 0, 0, 0, 0, 0],
+     [0, 0, 1, 0, 0, 0, 0, 0],
+     [0, 0, 0, 1, 0, 0, 0, 0],
+     [0, 0, 0, 0, 1, 0, 0, 0],
+     [0, 0, 0, 0, 0, 1, 0, 0],
+     [0, 0, 0, 0, 0, 0, 0, 1],
+     [0, 0, 0, 0, 0, 0, 1, 0]],
     dtype=torch.complex64)
