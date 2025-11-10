7b0000f2dbc19bb248964a0089d4219eacdd4751
Robin Cunningham <79066805+rspcunningham@users.noreply.github.com>
2025-11-10 11:37:33 -0500

    clean up

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-from quantum import QuantumSystem, Circuit, Gate
-from quantum.gates import H, CX, RY
+from quantum import QuantumSystem, Circuit, Gate, gates
 import torch
 
 theta = torch.pi / 2
 
 sys = QuantumSystem(2)
 print(sys)
-sys.apply_gate(H, [0])
+sys.apply_gate(gates.H, [0])
 print(sys)
-sys.apply_gate(CX, [0, 1])
+sys.apply_gate(gates.CX, [0, 1])
 print(sys)
-sys.apply_gate(RY(theta), [0])
+sys.apply_gate(gates.RY(theta), [0])
 print(sys)
 
 print("------------------------------------------------------------------------")
@@ -20,9 +19,9 @@ print(sys)
 c = Circuit(
     n_qubits=2,
     gates=[
-        Gate(H, [0]),
-        Gate(CX, [0, 1]),
-        Gate(RY(theta), [0])
+        Gate(gates.H, [0]),
+        Gate(gates.CX, [0, 1]),
+        Gate(gates.RY(theta), [0])
     ])
 
 sys.apply_circuit(c)
